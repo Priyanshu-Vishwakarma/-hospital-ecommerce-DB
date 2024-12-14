e5a6891348cf912c3b2f0554e745a7a76d25fec6
Priyanshu-Vishwakarma <git@stackblitz.com>
2024-12-14 22:45:46 +0530

    Initial commit

---
+++ models/ecommerce/product.models.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose"
+
+const ProductSchema = new mongoose.Schema({
+  description:{
+    type:String,
+    required:true
+  },name:{
+    type:String,
+    required:true,
+  },productImage:{
+    type:String,
+  },price:{
+    type:Number,
+    default:0
+  },stock:{
+    type:Number,
+    default:0
+  },category:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"Category",
+    required:true,
+  },Owner:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"User"
+  }
+},{timestamps:true});
+
+export const Product=mongoose.model("Product",ProductSchema)
---
+++ models/hospital-management/doctors.models.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const doctorSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    salary: {
+      type: String,
+      required: true,
+    },
+    qualification: {
+      type: String,
+      required: true,
+    },
+    experienceInYears: {
+      type: Number,
+      default: 0,
+    },worksinHospitals:[
+      {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Hospital"
+      },
+    ]
+  },
+  { timestamps: true }
+);
+
+export const Doctor = mongoose.model('Doctor', doctorchema);
